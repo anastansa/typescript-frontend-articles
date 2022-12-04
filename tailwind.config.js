e822d3a444089545040215de424ee185d4fceef2
Asya <119043723+anastansa@users.noreply.github.com>
2022-12-04 21:29:15 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    container: {
+      padding: '15px',
+      height: '100%',
+      maxWidth: '100%'
+    },
+    extend: {
+      colors: {
+       dark: '#1B2442',
+       main: '#ECEFF8',
+       blush: '#F9A8D4',
+       blue: '#abbff7'
+      },
+      boxShadow: {
+        'base': '0 0 8px 0 rgb(0 0 0 / 15%)',
+      }
+    }
+  },
+  plugins: [],
+}
+
